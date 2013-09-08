5c1d69802d46590bea332b719b92698ea106f35f
Kevin <kjeong0@gmail.com>
2013-09-07 21:29:13 -0400

    kevin stuff. including poll and stuff

--- public/js/modal.js
+++ public/js/modal.js
@@ -22,12 +22,17 @@ function initialize() {
   markersArray.push(marker);
 }
 
+var resultChosen = "";
+
 function showLocation(){
 	var location = document.getElementById('place').value;
 	geocoder.geocode( { 'address': location}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
 			clearOverlays();
-		    map.setCenter(results[0].geometry.location);
+		    resultChosen = {
+				name:results[0].formatted_address
+			}
+			map.setCenter(results[0].geometry.location);
 			var marker = new google.maps.Marker({
 				position: results[0].geometry.location,
 				map: map,
@@ -48,6 +53,23 @@ function clearOverlays() {
   markersArray = [];
 }
 
+function addToPoll(){
+	console.log('sup');
+	var done = false;
+	for (var i = 0;i<mockdata.selections.length&&!done;i++){
+		if (mockdata.selections[i].name==resultChosen.name){
+			mockdata.selections[i].votes++;
+			done = true;
+		}
+	}
+	if (!done){
+		mockdata.selections.push({name:resultChosen.name,votes:1});
+		console.log('added');
+	}
+	mockdata.selections[0].votes++;
+	pollrefresh();
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
@@ -61,3 +83,5 @@ $(function() {
 		pick12HourFormat: true
 	});
 });
+
+//Description
